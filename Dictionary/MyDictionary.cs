e6a74eb4f73a94def5d0a2c83081df45758a33cf
Kutluhan Kemal YAZICIOĞLU <kutluhan.yazicioglu@ogr.sakarya.edu.tr>
2021-01-23 17:39:31 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T>
+    {
+        T[] Array;
+        T[] tempArray;
+
+        public MyDictionary()
+        {
+            Array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempArray = Array;
+            Array = new T[Array.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                Array[i] = tempArray[i];
+            }
+            Array[Array.Length - 1] = item;
+
+        }
+        public void Count()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine(" {0}", Array[i]);
+            }
+
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> Words = new MyDictionary<string>();
+            Words.Add("Armut");
+            Words.Add("Bavul");
+            Words.Add("Cam");
+            Words.Add("Deniz");
+            Words.Add("Elbise");
+            Words.Count();
+
+
+      
+        
+           
+            
+        }
+    }
+}
